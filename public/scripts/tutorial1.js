d622a47bea7f168ecd84cf47bb6a727ea880855f
Parinthorn Saithong <parinthorn.sa@gmail.com>
2016-05-28 17:08:23 +0700

    Fetch data from server instead

--- public/scripts/tutorial1.js
+++ public/scripts/tutorial1.js
@@ -24,11 +24,31 @@ var CommentForm = React.createClass({
 	}
 });
 var CommentBox = React.createClass({
+	getInitialState: function() {
+		return {data: []};
+	},
+	loadCommentsFromServer: function() {
+		$.ajax({
+			url: this.props.url,
+			dataType: 'json',
+			cache: false,
+			success: function(data) {
+				this.setState({data: data});
+			}.bind(this),
+			error: function(xhr, status, err) {
+				console.error(this.props.url, status, err.toString());
+			}.bind(this)
+		});
+	},
+	componentDidMount: function() {
+		this.loadCommentsFromServer();
+		setInterval(this.loadCommentsFromServer, this.props.pollInterval);
+	},
 	render: function() {
 		return (
 			<div className="commentBox">
 				<h1>Comments</h1>
-				<CommentList data={this.props.data} />
+				<CommentList data={this.state.data} />
 				<CommentForm />
 			</div>
 		);
@@ -39,7 +59,6 @@ var Comment = React.createClass({
     var rawMarkup = marked(this.props.children.toString(), {sanitize: true});
     return { __html: rawMarkup };
   },
-	
 	render: function() {
 		return (
 			<div className="comment">
@@ -56,6 +75,6 @@ var data = [
 	{id: 2, author: "Jordan Walke", text: "This is *another* comment"}
 ];
 ReactDOM.render(
-	<CommentBox data={data} />,
+	<CommentBox url="/api/comments" pollInterval={2000} />,
 	document.getElementById('content')
 );
